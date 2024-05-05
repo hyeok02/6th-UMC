c2470953da967128e13e0d13c276c62e3ea57c4a
Mgby1201sh <ajin1991@naver.com>
2024-05-05 15:41:25 +0900

    Update Banner.jsx

--- week3/src/components/Banner/Banner.jsx
+++ week3/src/components/Banner/Banner.jsx
@@ -1,26 +1,26 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 const BannerContainer = styled.div`
-  background-color: #1e2125;
-  color: white;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  padding: 50px 20px;
-  height: 50%;
-  font-size: 20px;
-`;
+    width: 100%;
+    height: 17.5vw;
+    background-color: black;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`
 
-const WelcomeText = styled.h2`
-  margin: 0;
-`;
+const BannerP = styled.p`
+    color: white;
+    font-weight: bold;
+    font-size: 1.6vw;
+`
 
-function Banner() {
-  return (
-    <BannerContainer>
-      <WelcomeText>환영합니다</WelcomeText>
-    </BannerContainer>
-  );
+const Banner = () => {
+    return (
+        <BannerContainer>
+            <BannerP>환영합니다</BannerP>
+        </BannerContainer>
+    )
 }
 
 export default Banner;
